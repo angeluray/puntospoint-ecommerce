68e11bf082673db66dd9ad82f1f0ad5c5f428d09
Angel Uray <aeucampos@gmail.com>
2023-05-30 15:33:32 -0400

    Add counter cache to customer purchases

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < Account
+  has_many :sales, foreign_key: 'account_id', dependent: :destroy, inverse_of: :customer
+
+  # Add a counter cache for sales
+  counter_cache :purchases_count
 end
