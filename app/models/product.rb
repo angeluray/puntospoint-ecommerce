48eca50eb6e1e820e15baaa5062e9036bb8250a4
Angel Uray <aeucampos@gmail.com>
2023-05-30 14:57:20 -0400

    Set associations between Product and Categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :account
+  has_many :product_categorizations, dependent: :destroy
+  has_many :categories, through: :product_categorizations
+  has_many :sales
+
+  # Add counter cache for sales
+  counter_cache :sales_count
 end
