088038840e1ac514adb75973ae417c9008cb442f
Angel Uray <aeucampos@gmail.com>
2023-05-30 02:55:35 -0400

    Create overall admin entity

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :accounts
       resources :customers
+      resources :admins
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
