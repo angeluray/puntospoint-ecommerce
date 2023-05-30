ce7a92abfded1f77f8ca6d208c8b8e1fad9af9a3
Angel Uray <aeucampos@gmail.com>
2023-05-30 18:11:19 -0400

    Modify account entity

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,9 @@
 class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
     has_many :products, dependent: :destroy
     has_many :sales
     has_many :categories
