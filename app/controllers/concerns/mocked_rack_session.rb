d3d47c21bbadc9cc3289f8a4f46a36af92a487ad
Angel Uray <aeucampos@gmail.com>
2023-05-30 23:27:34 -0400

    Finish mocked session module

---
+++ app/controllers/concerns/mocked_rack_session.rb
@@ -0,0 +1,23 @@
+module MockedRackSession
+  extend ActiveSupport::Concern
+  
+  class MockSession < Hash 
+    def enabled?
+      false
+    end
+
+    def destroy; end
+  end
+
+  included do
+    before_action :set_mocked_session
+
+    private
+
+    def set_mocked_session
+        request.env['rack.session'] ||= MockSession.new
+    end
+  end
+
+
+end
