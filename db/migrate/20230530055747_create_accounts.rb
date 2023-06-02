e9b672693e0f9640a3ccf350c27456f234d81205
Angel Uray <aeucampos@gmail.com>
2023-06-01 23:31:19 -0400

    Fix SITamong Account, Customer and Admins

--- db/migrate/20230530055747_create_accounts.rb
+++ db/migrate/20230530055747_create_accounts.rb
@@ -3,10 +3,15 @@ class CreateAccounts < ActiveRecord::Migration[7.0]
     create_table :accounts do |t|
       t.string :first_name
       t.string :last_name
+      t.string :username
       t.string :password
       t.string :password_confirmation
       t.string :auth_token
+      t.string :address
+      t.decimal :balance, default: 0
+      t.integer :purchases_count
       t.string :type
+      t.integer :role_level
 
       t.timestamps
     end
