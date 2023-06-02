c08408b19aede775ebb42359b7090402cc99f45e
Angel Uray <aeucampos@gmail.com>
2023-06-01 23:39:09 -0400

    Update models logic

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :account
+  has_many :images, as: :photolink
   has_many :product_categorizations, dependent: :destroy
   has_many :categories, through: :product_categorizations
   has_many :sales
 
-  # Add counter cache for sales
-  counter_cache :sales_count
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 3000 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :product_stocks, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :account_id, presence: true
 end
