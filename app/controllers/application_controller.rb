28774cdb49a8b25b2dcff78d1e0899a58bc5df71
Angel Uray <aeucampos@gmail.com>
2023-06-01 23:19:58 -0400

    Add space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name type address username role_level password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name type address username role_level password_confirmation])
